23ba10e9e0b79482e3483b43b49884ff00cfca72
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-10-30 19:30:35 +0100

    Cleaned out useless function calls

--- apps/service-notification/src/email/email.service.ts
+++ apps/service-notification/src/email/email.service.ts
@@ -2,54 +2,19 @@ import { Injectable, Logger } from '@nestjs/common';
 import { SendGridService } from '@anchan828/nest-sendgrid';
 import { UserEntity } from '@graphqlcqrs/repository/entities';
 import { ConfigService } from '@graphqlcqrs/common/services/config.service';
+import { MailData } from '@sendgrid/helpers/classes/mail';
 
 @Injectable()
 export class EmailService {
   constructor(
     private readonly sendGrid: SendGridService,
-    private readonly configService: ConfigService,
   ) {}
 
-  public async sendLoggedInEmail(user: UserEntity) {
-    const userEmail = user.emails.reduce(previousValue => previousValue.primary === true && previousValue);
+  public async sendEmail(data: Partial<MailData> | Array<Partial<MailData>>, isMultiple?: boolean) {
+    if (data) { return null; }
 
     try {
-      await this.sendGrid.send({
-        to: userEmail.address,
-        from: 'noreply@demo.com',
-        subject: 'Testing Nest MailerModule ✔',
-        text: 'You just logged into this device', // plaintext body
-      });
-    } catch (e) {
-      Logger.error(e.message, e);
-    }
-  }
-
-  public async sendRegisterEmail(user: UserEntity) {
-    const userEmail = user.emails.reduce(previousValue => previousValue.primary === true && previousValue);
-
-    try {
-      await this.sendGrid.send({
-        to: userEmail.address,
-        from: 'noreply@demo.com',
-        subject: 'Thank for registering',
-        text: `Here is your verification code ${userEmail.verificationCode}`, // plaintext body
-      });
-    } catch (e) {
-      Logger.error(e.message, e);
-    }
-  }
-
-  public async sendWelcomeEmail(user: UserEntity) {
-    const userEmail = user.emails.reduce(previousValue => previousValue.primary === true && previousValue);
-
-    try {
-      await this.sendGrid.send({
-        to: userEmail.address,
-        from: 'noreply@demo.com',
-        subject: 'Hurray! Welcome',
-        text: `Your account has been verified successfully, you can now log in.`, // plaintext body
-      });
+      await this.sendGrid.send({ ...data, from: 'noreply@demo.com' }, isMultiple);
     } catch (e) {
       Logger.error(e.message, e);
     }
