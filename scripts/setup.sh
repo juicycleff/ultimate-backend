2eb2b67fa7db43bd37c269b8578ac71f3d190f53
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-12-29 07:00:54 +0100

    update readme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,8 @@ PROJECTS=( $(cat ./nest-cli.json | jq -r '.projects[].root'))
 echo "Service and Library build system started"
 
 for VAR in "${PROJECTS[@]}" ; do
-  echo "Building for" ${VAR:5}
-  npx nest build ${VAR:5}
+  echo "Building for" "${VAR:5}"
+  npx nest build "${VAR:5}"
 done
 
 echo "Service and Library build system completed"
