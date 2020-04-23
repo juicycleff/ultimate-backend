e27c61ebd00ab52128518914cafcd427cda1b0e4
Maxime Trimolet <maxime.trimolet@gmail.com>
2020-04-23 10:11:55 +0200

    Improve script for configuration

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # shellcheck disable=SC2207
 PROJECTS=( $(jq -r '.projects[] | select(.type == "application") | .root' ./nest-cli.json) )
+BOOTSTRAP_PATH=src/bootstrap.yaml
+CONFIG_PATH=config.yaml
 
 echo "Service Registration system started"
 
 for PROJECT_DIR in "${PROJECTS[@]}" ; do
-  if [ ! -f "./${PROJECT_DIR}/src/bootstrap.yaml" ]; then
-    echo "./${PROJECT_DIR}/src/bootstrap.yaml not found, skipping service"
+  if [ ! -f "./${PROJECT_DIR}/${BOOTSTRAP_PATH}" ]; then
+    echo "./${PROJECT_DIR}/${BOOTSTRAP_PATH} not found, skipping service"
     continue
   fi
 
-  SVC_NAME=$(yq r ./${PROJECT_DIR}/src/bootstrap.yaml 'service.name' )
+  SVC_NAME=$(yq r ./${PROJECT_DIR}/${BOOTSTRAP_PATH} 'service.name' )
   echo "Registering ${SVC_NAME}"
 
-  if [ ! -f "./${PROJECT_DIR}/config.example" ]; then
-    echo "./${PROJECT_DIR}/config.example not found, skipping service"
+  if [ ! -f "./${PROJECT_DIR}/${CONFIG_PATH}" ]; then
+    echo "./${PROJECT_DIR}/${CONFIG_PATH} not found, skipping service"
     continue
   fi
-  consul kv put ultimatebackend/config/${SVC_NAME} \@./${PROJECT_DIR}/config.example
+  consul kv put ultimatebackend/config/${SVC_NAME} \@./${PROJECT_DIR}/${CONFIG_PATH}
 done
 
 echo "Service Registration system completed"
