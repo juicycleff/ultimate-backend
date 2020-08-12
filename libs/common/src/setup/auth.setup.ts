9f7cd12bf5651abc628ba847368310798d9b0bbd
Rex Raphael <juicycleff@gmail.com>
2020-08-12 14:32:53 +0200

    Update auth.setup.ts

--- libs/common/src/setup/auth.setup.ts
+++ libs/common/src/setup/auth.setup.ts
@@ -8,64 +8,58 @@ import * as connectRedis from 'connect-redis';
 import { Express } from 'express';
 import { NestCloud } from '@nestcloud/core';
 
-// tslint:disable-next-line:no-var-requires
 const parseIsoDuration = require('parse-iso-duration');
 
 const RedisStore = connectRedis(session);
-
-// @ts-ignore
-export const passportMiddleware = passport.initialize();
-export const passportSessionMiddleware = passport.session();
-
-// @ts-ignore
-export function getSessionStoreMiddleware() {
-  const redisConfig: redis.ClientOpts = NestCloud.global.boot.get(
-    'redis',
-    {},
-  );
-  const authConfig: {
-    sessionKey?: string;
-    sessionSecret?: string;
-    domain?: string;
-    resave?: boolean;
-    rolling?: boolean;
-    saveUninitialized?: boolean;
-    cookieMaxAge?: string;
-    secure?: boolean;
-  } = NestCloud.global.boot.get('app.auth', {});
-  if (redisConfig?.password === '') {
-    delete redisConfig.password;
-  }
-  // redis init
-  const redisClient = redis.createClient({
-    ...redisConfig,
-  });
-  const store = new RedisStore({ client: redisClient });
-
-  // sessions
-  return session({
-    secret: authConfig.sessionSecret,
-    resave: Boolean(authConfig?.resave),
-    rolling: Boolean(authConfig?.rolling),
-    saveUninitialized: false,
-    cookie: {
-      maxAge: parseIsoDuration('PT' + authConfig.cookieMaxAge),
-      secure: Boolean(authConfig?.secure),
-      domain: authConfig?.domain,
-    },
-    // proxy: withPassport === true ? undefined : true,
-    store,
-    name: authConfig.sessionKey,
-  });
-}
+const passportMiddleware = passport.initialize();
+const passportSessionMiddleware = passport.session();
 
 export function authSetup(
   app: INestApplication | Express,
   withPassport: boolean = true,
 ) {
   try {
+    const redisConfig: redis.RedisOptions = NestCloud.global.boot.get(
+      'redis',
+      {},
+    );
+    const authConfig: {
+      sessionKey?: string;
+      sessionSecret?: string;
+      domain?: string;
+      resave?: boolean;
+      rolling?: boolean;
+      saveUninitialized?: boolean;
+      cookieMaxAge?: string;
+      secure?: boolean;
+    } = NestCloud.global.boot.get('app.auth', {});
+
+    if (redisConfig?.password === '') {
+      delete redisConfig.password;
+    }
+
+    // redis init
+    const redisClient = redis.createClient({
+      ...redisConfig,
+    });
+
+    const store = new RedisStore({ client: redisClient });
+
     // sessions
-    const sessionMiddleware = getSessionStoreMiddleware();
+    const sessionMiddleware = session({
+      secret: authConfig.sessionSecret,
+      resave: Boolean(authConfig?.resave),
+      rolling: Boolean(authConfig?.rolling),
+      saveUninitialized: false,
+      cookie: {
+        maxAge: parseIsoDuration('PT' + authConfig.cookieMaxAge),
+        secure: Boolean(authConfig?.secure),
+        domain: authConfig?.domain,
+      },
+      // proxy: withPassport === true ? undefined : true,
+      store,
+      name: authConfig.sessionKey,
+    });
 
     if (withPassport) {
       // @ts-ignore
