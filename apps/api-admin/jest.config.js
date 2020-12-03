b7dec29a80d539059aa775bf8207a9d6f429d4ae
Rex Isaac Raphael <rex.raphael@outlook.com>
2020-12-03 22:39:06 +0100

    initial commit

---
+++ apps/api-admin/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'api-admin',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api-admin',
+};
