2a82ac59861a8aeaea91034f50987c13c677c12f
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-07-16 20:22:11 +0200

    chore(core): fixed missing rpc exception

--- packages/core/src/lib/exceptions/rpc.error.ts
+++ packages/core/src/lib/exceptions/rpc.error.ts
@@ -1,12 +1,31 @@
 /* tslint:disable:max-classes-per-file */
-
 import { ServiceError } from './grpc.error';
+import { isObject, isString } from '@nestjs/common/utils/shared.utils';
 
-export declare class RpcException extends Error {
-  private readonly error;
-  constructor(error: string | object);
-  initMessage(): void;
-  getError(): string | object;
+export class RpcException extends Error {
+  constructor(private readonly error: string | object) {
+    super();
+    this.initMessage();
+  }
+
+  public initMessage() {
+    if (isString(this.error)) {
+      this.message = this.error;
+    } else if (
+      isObject(this.error) &&
+      isString((this.error as Record<string, any>).message)
+    ) {
+      this.message = (this.error as Record<string, any>).message;
+    } else if (this.constructor) {
+      this.message = this.constructor.name
+        .match(/[A-Z][a-z]+|[0-9]+/g)
+        .join(' ');
+    }
+  }
+
+  public getError(): string | object {
+    return this.error;
+  }
 }
 
 export enum GrpcExceptionStatus {
