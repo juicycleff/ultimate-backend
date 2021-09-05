ede8145e6704ef4cdad6ba5a920bc943079b7b25
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-09-05 23:05:41 +0200

    chore: downgraded apollo-server-express dep

--- examples/example/src/main.ts
+++ examples/example/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { UBServiceFactory } from '@ultimate-backend/core';
-
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
 
 /**
@@ -8,12 +8,15 @@ import { AppModule } from './app/app.module';
  * This is only a minimal backend to get started.
  */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   await UBServiceFactory.create(app)
     .withSwagger()
     .withGrpc()
-    .withPoweredBy()
+    // .withPoweredBy()
     .withMultiTenancy()
     .withPrefix('api')
     .start(parseInt(process.env.PORT, 10) || 3332);
